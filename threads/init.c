c31584724376021f73040eb924b45a35e926e46b
ksb21st <21supersoo@kaist.ac.kr>
2021-03-15 11:26:46 +0900

    power_off_when_done

--- threads/init.c
+++ threads/init.c
@@ -204,6 +204,8 @@ read_command_line (void) {
    and returns the first non-option argument. */
 static char **
 parse_options (char **argv) {
+	//edit
+	power_off_when_done = true;
 	for (; *argv != NULL && **argv == '-'; argv++) {
 		char *save_ptr;
 		char *name = strtok_r (*argv, "=", &save_ptr);
